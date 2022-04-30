25d65ed13cbb252293a996b2dc88090a92704242
rswatriumvir <roswain2002@gmail.com>
2022-04-30 14:31:48 -0400

    Update clean_data.R

--- R/clean_data.R
+++ R/clean_data.R
@@ -18,14 +18,13 @@ names(prim_16)[5] = "sanders_perc_16"
 names(prim_16)[8] = "turnout_16"
 prim_16[9] = NULL
 names(prim_16)[7] = "total_vote_16" 
-names(prim_16)[2] = "biden_raw_16"
 names(prim_16)[4] = "sanders_raw_16"
-names(prim_16)[2] = "others_raw_16"
+names(prim_16)[2] = "clinton_raw_16"
 prim_16 <- prim_16[-c(47),]
 
 content_20 = read_html("https://en.wikipedia.org/wiki/2020_South_Carolina_Democratic_presidential_primary")
 table_20 = html_table(content_20, fill = TRUE)
-prim_20 = table_20[[13]]
+prim_20 = table_20[[14]]
 names(prim_20)[3] = "biden_perc_20" 
 names(prim_20)[5] = "buttigieg_perc_20" 
 names(prim_20)[7] = "gabbard_perc_20" 
@@ -50,11 +49,18 @@ prim_20[18] = NULL
 ##merging dataset --------------------------------------------------------------
 sc_prim <- merge(prim_16, prim_20)
 sc_prim$turnout_16 = gsub("%", "", sc_prim$turnout_16)
+sc_prim$total_vote_16 = gsub(",", "", sc_prim$total_vote_16)
+sc_prim$total_votes_20 = gsub(",", "", sc_prim$total_votes_20)
 
 sc_prim$turnout_16 = as.numeric(sc_prim$turnout_16)
+sc_prim$total_votes_20 = as.numeric(sc_prim$total_votes_20)
+sc_prim$total_vote_16 = as.numeric(sc_prim$total_vote_16)
+
 
 ##calculations -------------------------------------------------------------------
 sc_prim$turnout_diff = sc_prim$turnout_20 - sc_prim$turnout_16
+sc_prim$total_diff = sc_prim$total_votes_20 - sc_prim$total_vote_16
+sc_prim$perc_coal = sc_prim$total_diff/sum(sc_prim$total_diff)
 
 ##mapping -----------------------------------------------------------------
 counties_sf = get_urbn_map("counties", sf = TRUE)
